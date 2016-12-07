35bc93a11ec9127e2776d94ff7ecfc0a3d7cb9dc
Eric Krause and Tim Jung <eric@>
2016-12-07 11:34:22 -0800

    create contact list for users to message

---
+++ app/src/main/java/com/example/guest/chatapplication/models/User.java
@@ -0,0 +1,22 @@
+package com.example.guest.chatapplication.models;
+
+/**
+ * Created by Guest on 12/7/16.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
